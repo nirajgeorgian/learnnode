80b5dd434e72a5e695ea681763833def99f71adb
nirajgeorgian <nirajgeorgian@gmail.com>
2018-09-08 20:50:47 +0530

    added express

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/html"})
-  res.end("<h1>Hello World</h1>")
+app.get('/', (req, res) => {
+  res.send({
+    "hello": "World"
+  })
 })
 
-server.listen(3000)
+app.get('/dodo', (req, res) => {
+  // make some database query
+  //store the database in xyz variable
+  const dbreturn = {
+    "name": "dodo",
+    "age": 12,
+    "gender": "F"
+  }
+
+  res.send(dbreturn)
+
+})
+
+app.listen(3000, (err) => {
+  if(err) {
+    console.log(err)
+  }
+  console.log("Running on http://localhost:3000")
+})
